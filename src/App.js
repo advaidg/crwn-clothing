20ec963d2c29928889eca42b73936576935e9d37
Advaid Gireesan <73230570+advaidg@users.noreply.github.com>
2021-04-26 14:21:54 +0530

    enu style

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import HomePage from './Homepage/homepage.component';
+import HomePage from "./pages/homepage/homepage.component.jsx"
 import './App.css';
 
 function App() {
